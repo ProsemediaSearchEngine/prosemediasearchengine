c29cd27a584f44705c256ad37970b22a202c4d72
Shubham yeole <shubham20.yeole@gmail.com>
2017-03-09 17:44:41 -0500

    Update display functionality

---
+++ public/js/demo.js
@@ -0,0 +1,36 @@
+$(document).on("keyup",'#demo1_text',function() {
+    var max = 300;
+    var entry = $(this).val();
+    var length = entry.length;
+    var charRemaining = max-length;
+    if(charRemaining<=0){
+        $("#demo1_err").html(charRemaining+" character remaining. <p class='errmsg'>You reached maximum characters limit.</p>");
+    }else{
+        $("#demo1_err").html(charRemaining+" character remaining.");
+    }
+});
+
+$(document).on("keyup",'.updatepost',function() {
+	var classname = $(this).attr("class");
+	var id = $(this).attr("id");
+	var text = $(this).val();
+    var res = classname.split("_");
+    var coloumn = res[0];
+	$.post( "/updateDemo", { id: id, coloumn: coloumn, text: text})
+      .done(function(result){
+        $("#u_message_"+id).html("Update done.");
+	});
+});
+
+$( document ).ready(function() {
+	$(".add_new_demo_div").hide();
+	$(".update_demo_div").hide();
+	$( ".add_new_demo_button" ).click(function() {
+	  	$(".add_new_demo_div").toggle("slow");
+	});
+
+	$( ".update_demo_btn" ).click(function() {
+		var id = $(this).data("id");
+	  	$("#hidden_"+id).toggle("slow");
+	});
+});
